8aa4b4da5d00a9c4b023236d8aae886d5988e9cc
Viihh-77 <gabrielli_glowatski@estudante.sesisenai.org.br>
2025-09-17 20:11:28 -0300

    Chore: adicionando escolha no service

--- Service/Stock.java
+++ Service/Stock.java
@@ -1,16 +1,57 @@
 package Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import Model.Hospede;
+import Model.Quarto;
+import Model.Reserva;
+import Model.Servico;
+
 import View.Attendant;
 
 public class Stock {
 
-    public void gerenciarEstoque (Attendant attendant, int opcaoEscolhida){
+    List<Hospede> hospedes;
+    List<Quarto> quartos;
+    List<Reserva> reservas;
+    List<Servico> servicos;
 
-
-
-
+    public Stock() {
+        hospedes = new ArrayList<>();
+        quartos = new ArrayList<>();
+        reservas = new ArrayList<>();
+        servicos = new ArrayList<>();
     }
 
+    public void gerenciarEstoque (Attendant attendant, int opcaoEscolhida){
 
+        switch (opcaoEscolhida) {
+        
+            case 1:
+                Attendant.MenuCadastro();
+                break;
 
+            case 2:
+                Attendant.MenuListar();
+                break;
+
+            case 3:
+                Attendant.menuPesquisar();
+                break;
+
+            case 4:
+                Attendant.editarDados();
+                break;
+
+            case 5:
+                Attendant.cancelarReserva();
+                break;
+
+            case 6:
+                Attendant.sairSistema();
+                break;
+
+        }
+    }
 }
